acc273bdd559336eda4035c765a83fff68e26f9c
rgolder1 <rgolder1@yahoo.com>
2024-03-15 15:08:59 +0000

    Initial project.

--- src/test/java/demo/repository/TestItemRepository.java
+++ src/test/java/demo/repository/TestItemRepository.java
@@ -1,6 +1,5 @@
-package demo.component.repository;
+package demo.repository;
 
-import demo.repository.ItemRepository;
 import io.micronaut.context.annotation.Replaces;
 import io.micronaut.data.jdbc.annotation.JdbcRepository;
 import io.micronaut.data.model.query.builder.sql.Dialect;
